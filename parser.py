74954eb33872317727668ed9bc84f9c7eab0e127
Mehdi Moulay-Rchid <memoul@free.fr>
2018-09-05 11:15:37 +0200

    Delete parser.py

--- parser.py
+++
@@ -1,9 +0,0 @@
-from light_parser import Parser, BlockStatement, Statement, Term, Seq, Any, Optional, Repeat, Node, Or
-from light_parser import NodePath, AnyPath, Token
-from pygments.lexers.dotnet import VbNetLexer
-
-def parse(text):
-    
-    pass
-    
-    
